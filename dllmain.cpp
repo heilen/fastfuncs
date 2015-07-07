cb93f81a044c39428b053dcc0963968adebf0471
Adrian Gonzalez <adrian@heilen.com.ar>
2015-07-07 16:32:04 -0300

    Migrated to VS 2013 + Removed crash hook

--- dllmain.cpp
+++ dllmain.cpp
@@ -937,14 +937,14 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		Install_Crash_Hook();
+		//Install_Crash_Hook();
 		break;
 	case DLL_THREAD_ATTACH:
 		break;
 	case DLL_THREAD_DETACH:
 		break;
 	case DLL_PROCESS_DETACH:
-		Uninstall_Crash_Hook();
+		//Uninstall_Crash_Hook();
 		break;
 	}
 	return TRUE;
--- libs/build_libs.bat
+++ libs/build_libs.bat
@@ -1,4 +1,5 @@
-set PATH=C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\;c:\Program Files (x86)\Microsoft Visual Studio 9.0\vc\bin\;%PATH%
+@ECHO OFF
+set PATH=c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\;c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\;%PATH%
 
 lib /DEF:pbshr100.def /OUT:pbshr100.lib
 lib /DEF:pbshr105.def /OUT:pbshr105.lib
@@ -13,3 +14,5 @@ lib /DEF:pbvm110.def /OUT:pbvm110.lib
 lib /DEF:pbvm115.def /OUT:pbvm115.lib
 lib /DEF:pbvm120.def /OUT:pbvm120.lib
 lib /DEF:pbvm125.def /OUT:pbvm125.lib
+
+pause
